b3339b738da93e8678095f723954131f3369e9fb
bclayton314 <62265654+bclayton314@users.noreply.github.com>
2020-06-06 11:18:13 -0500

    Add files via upload

---
+++ HackerRank_Easy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SOCK MERCHANT
+            //int n = int.Parse(Console.ReadLine());
+            //int[] A = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            //Array.Sort(A);
+            //int ans = 0;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if (A[i] == A[i - 1])
+            //    {
+            //        ans++;
+            //        i++;
+
+            //    }
+            //}
+            //Console.WriteLine(ans);
+
+            //REPEATED STRING
+            //string str1 = Console.ReadLine();
+            //long n = long.Parse(Console.ReadLine());
+            
+            //var counter1 = 0;
+            //var counter2 = 0;
+            //var part1 = n / str1.Length;
+            //var part2 = n % str1.Length;
+            //for (int i = 0; i < str1.Length; i++)
+            //{
+            //    if (str1[i] == 'a')
+            //    {
+            //        counter1++;
+            //        if (i < part2)
+            //        {
+            //            counter2++;
+            //        }
+
+            //    }
+            //}
+            //var result = (counter2 * part1) + part2;
+            //Console.WriteLine(result);
+
+        }
+    }
+}
